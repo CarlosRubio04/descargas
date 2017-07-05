7ce504d83ed62a1d3980507ecb156094495af052
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-07-05 18:07:47 -0500

    Lay out

---
+++ blocks/home.php
@@ -0,0 +1,61 @@
+<div class="wrapper">
+	<div class="row-wrapper">
+		<h2>Juegos populares</h2>
+	</div>
+	<div class="row-wrapper flex">
+		<div class="game-item">
+			<img src="http://placehold.it/170x130" alt="Game" class="thumbs">
+			<h4>Monster Truck</h4>
+			<p>Descripción del video juego o posibles hashtags</p>
+			<div class="rate">
+				
+			</div>
+		</div>
+
+		<div class="game-item">
+			<img src="http://placehold.it/170x130" alt="Game" class="thumbs">
+			<h4>Monster Truck</h4>
+			<p>Descripción del video juego o posibles hashtags</p>
+			<div class="rate">
+				
+			</div>
+		</div>
+
+		<div class="game-item">
+			<img src="http://placehold.it/170x130" alt="Game" class="thumbs">
+			<h4>Monster Truck</h4>
+			<p>Descripción del video juego o posibles hashtags</p>
+			<div class="rate">
+				
+			</div>
+		</div>
+
+		<div class="game-item">
+			<img src="http://placehold.it/170x130" alt="Game" class="thumbs">
+			<h4>Monster Truck</h4>
+			<p>Descripción del video juego o posibles hashtags</p>
+			<div class="rate">
+				
+			</div>
+		</div>
+
+
+		<div class="game-item">
+			<img src="http://placehold.it/170x180" alt="Game" class="thumbs">
+			<h4>Monster Truck</h4>
+			<p>Descripción del video juego o posibles hashtags</p>
+			<div class="rate">
+				
+			</div>
+		</div>
+
+		<div class="game-item">
+			<img src="http://placehold.it/170x190" alt="Game" class="thumbs">
+			<h4>Monster Truck</h4>
+			<p>Descripción del video juego o posibles hashtags</p>
+			<div class="rate">
+				
+			</div>
+		</div>
+	</div>
+</div>
---
+++ blocks/sidebar.php
@@ -0,0 +1,5 @@
+<div class="side-wrapper">
+	<div class="banner">
+		<img src="http://placehold.it/480x320" alt="Publicidad" class="side-banner">
+	</div>
+</div>
